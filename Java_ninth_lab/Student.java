8d4f2e910959fbb24ef92c795bd5715ed142079b
Victoria Tabakova <vrtabakova@gmail.com>
2023-12-10 21:11:05 +0300

    Add ninth lab

---
+++ Java_ninth_lab/Student.java
@@ -0,0 +1,30 @@
+public class Student {
+    private String first_name;
+    private String last_name;
+    private int age;
+    private double average_score;
+
+    Student() {
+        this.first_name = "";
+        this.last_name = "";
+        this.age = 0;
+        this.average_score = 0;
+    };
+
+    Student(String first_name, String last_name, int age, double average_score) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.age = age;
+        this.average_score = average_score;
+    };
+
+    @Override
+    public String toString() {
+        return "First name \"" + this.first_name + "\": Last name = " + this.last_name + ", Age = " + this.age
+                + ", score = " + this.average_score;
+    };
+
+    public void greet() {
+        System.out.println("Hi, I am " + this.first_name);
+    };
+};
