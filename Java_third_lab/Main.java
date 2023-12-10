b61c1392db9b9e3ad4e11a7598452509a96988c4
Victoria Tabakova <vrtabakova@gmail.com>
2023-12-10 21:02:55 +0300

    Add third lab

---
+++ Java_third_lab/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+  public static void main(String[] args) {
+  
+    System.out.println(true && false);
+    System.out.println(true || false);
+    System.out.println(!true);
+
+    System.out.println(6 > 8 ? "Yes" : "No");
+
+    System.out.println(6 & 8);
+    System.out.println(6 | 8);
+    System.out.println(6 ^ 8);
+    System.out.println(~6);
+
+    System.out.println(3 >> 8);
+    System.out.println(-6 >> 3);
+    System.out.println(3 >>> 8);
+    System.out.println(-6 >>> 8);
+  };
+};
